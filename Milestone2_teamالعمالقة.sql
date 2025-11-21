06a9eb02ee6ab97897c97b7964f207e03d838353
Metwaly-beh <adhammetwaly25@gmail.com>
2025-11-21 14:33:41 +0200

    Organization + formating some parts

--- Milestone2_teamالعمالقة.sql
+++ Milestone2_teamالعمالقة.sql
@@ -1,3 +1,6 @@
+
+/*2.1 Omar Mohamed Starts here*/
+
 CREATE DATABASE University_HR_ManagementSystem_Team_40
 GO
 USE University_HR_ManagementSystem_Team_40
@@ -13,7 +16,7 @@ CHECK (name IN ('MET', 'IET', 'HR', 'Medical')),
 );
 
 CREATE TABLE Employee(
-employee_ID INT IDENTITY(1,1),
+employee_ID INT PRIMARY KEY IDENTITY(1,1),
 first_name VARCHAR(50),
 last_name VARCHAR(50),
 email VARCHAR(50),
@@ -32,22 +35,19 @@ accidental_balance INT,
 salary DECIMAL(10,2),
 hire_date DATE,
 last_working_date DATE,
-dept_name VARCHAR(50),
+dept_name VARCHAR(50) FOREIGN KEY REFERENCES Department(name),
 CHECK(employment_status IN ('active','onleave','notice_period','resigned')),
-CHECK(type_of_contract IN ('full_time','part_time')),
-CONSTRAINT PK_EMP PRIMARY KEY (employee_ID),
-CONSTRAINT FK_EMP_DEP FOREIGN KEY (dept_name) REFERENCES Department
+CHECK(type_of_contract IN ('full_time','part_time'))
 );
 
 CREATE TABLE Employee_Phone (
-emp_ID INT ,
+emp_ID INT FOREIGN KEY REFERENCES Employee(employee_ID),
 phone_num CHAR(11),
-CONSTRAINT PK_EMP_PHONE PRIMARY KEY (emp_ID,phone_num),
-CONSTRAINT FK_EMP_EmpPhone FOREIGN KEY (emp_ID) REFERENCES Employee(employee_ID)
+PRIMARY KEY(emp_ID,phone_num)
 );
 
 CREATE TABLE Role(
-role_name VARCHAR(50),
+role_name VARCHAR(50) PRIMARY KEY,
 title VARCHAR(50),
 description VARCHAR(50),
 rank INT,
@@ -55,179 +55,143 @@ base_salary DECIMAL(10,2),
 percentage_YOE DECIMAL(4,2),
 percentage_overtime DECIMAL(4,2),
 annual_balance INT,
-accidental_balance INT,
-CONSTRAINT PK_ROLE PRIMARY KEY (role_name)
+accidental_balance INT
 );
 
 CREATE TABLE Employee_Role(
-emp_ID INT,
-role_name VARCHAR(50),
-CONSTRAINT PK_EMP_ROLE PRIMARY KEY (emp_ID,role_name),
-CONSTRAINT FK_EMP_EmpRole1 FOREIGN KEY (emp_ID) REFERENCES Employee(employee_ID),
-CONSTRAINT FK_EMP_EmpRole2 FOREIGN KEY (role_name) REFERENCES Role(role_name)
+emp_ID INT FOREIGN KEY REFERENCES Employee(employee_ID),
+role_name VARCHAR(50)  FOREIGN KEY REFERENCES Role(role_name),
+PRIMARY KEY (emp_ID,role_name)
 );
 
 CREATE TABLE Role_existsIn_Department(
-department_name varchar(50),
-role_name varchar(50),
-CONSTRAINT PK_ROLE_DEP PRIMARY KEY (department_name,role_name),
-CONSTRAINT FK_DEP_ROLE1 FOREIGN KEY (department_name) REFERENCES Department(name),
-CONSTRAINT FK_DEP_ROLE2 FOREIGN KEY (role_name) REFERENCES Role(role_name)
+department_name varchar(50) FOREIGN KEY REFERENCES Department(name),
+role_name varchar(50) FOREIGN KEY REFERENCES Role(role_name),
+PRIMARY KEY (department_name,role_name)
 );
 
 CREATE TABLE Leave (
-    request_ID INT IDENTITY(1,1), 
+    request_ID INT IDENTITY(1,1) PRIMARY KEY, 
     date_of_request DATE,
     start_date DATE,
     end_date DATE,
     num_days AS DATEDIFF(DAY, start_date, end_date),
     final_approval_status VARCHAR(50) DEFAULT 'pending',
-    CONSTRAINT CK_Leave_ApprovalStatus 
-    CHECK (final_approval_status IN ('approved', 'rejected', 'pending')),
-    CONSTRAINT PK_Leave PRIMARY KEY (request_ID)
+    CHECK (final_approval_status IN ('approved', 'rejected', 'pending'))
 );
 
 CREATE TABLE Annual_Leave(
-request_ID int,
-emp_ID int,
-replacement_emp int
-CONSTRAINT PK_Annual_Leave PRIMARY KEY (request_ID),
-CONSTRAINT FK_Annual_Emp1 FOREIGN KEY (emp_ID) REFERENCES Employee(employee_ID),
-CONSTRAINT FK_Annual_Leave FOREIGN KEY (request_ID) REFERENCES Leave(request_ID),
-CONSTRAINT FK_Annual_Emp2 FOREIGN KEY (replacement_emp) REFERENCES Employee(employee_ID)
+request_ID int FOREIGN KEY REFERENCES Leave(request_ID),
+emp_ID int FOREIGN KEY REFERENCES Employee(employee_ID),
+replacement_emp int FOREIGN KEY REFERENCES Employee(employee_ID),
+PRIMARY KEY (request_ID)
 );
 
 CREATE TABLE Accidental_Leave
 (
-request_ID int,
-emp_ID int,
-CONSTRAINT PK_Accidental_Leave PRIMARY KEY (request_ID),
-CONSTRAINT FK_Accidental_Emp FOREIGN KEY (emp_ID) REFERENCES Employee(employee_ID),
-CONSTRAINT FK_Accidental_Leave FOREIGN KEY (request_ID) REFERENCES Leave(request_ID)
+request_ID int FOREIGN KEY REFERENCES Leave(request_ID),
+emp_ID int FOREIGN KEY  REFERENCES Employee(employee_ID),
+PRIMARY KEY (request_ID)
 );
 
 CREATE TABLE Medical_Leave  (
-    request_ID  INT,
+    request_ID  INT FOREIGN KEY REFERENCES Leave(request_ID),
     insurance_status BIT,
     disability_details VARCHAR(50),
     type VARCHAR(50),
-    Emp_ID INT,
-    CONSTRAINT PK_Medical_Leave PRIMARY KEY (request_ID),
-    CONSTRAINT FK_Medical_Request FOREIGN KEY (request_ID) REFERENCES Leave(request_ID),
-    CONSTRAINT FK_Medical_Employee FOREIGN KEY(Emp_ID) REFERENCES Employee(employee_ID),
-    CONSTRAINT CK_Medical_Type CHECK (type IN ('sick', 'maternity'))
+    CHECK (type IN ('sick', 'maternity')),
+    Emp_ID INT FOREIGN KEY REFERENCES Employee(employee_ID),
+    PRIMARY KEY (request_ID)
 );
 
 CREATE TABLE Unpaid_Leave (
-    request_ID  INT ,
-    Emp_ID INT, 
-    CONSTRAINT PK_Unpaid_Leave PRIMARY KEY (request_ID),
-    CONSTRAINT FK_Unpaid_Leave FOREIGN KEY (request_ID) REFERENCES Leave(request_ID),
-    CONSTRAINT FK_Unpaid_Employee FOREIGN KEY (Emp_ID) REFERENCES Employee(employee_ID)
+    request_ID  INT FOREIGN KEY  REFERENCES Leave(request_ID) ,
+    Emp_ID INT FOREIGN KEY  REFERENCES Employee(employee_ID), 
+    PRIMARY KEY (request_ID)
 );
 
 CREATE TABLE Compensation_Leave (
-    request_ID  INT,
+    request_ID  INT FOREIGN KEY  REFERENCES Leave(request_ID),
     reason VARCHAR(50), 
     date_of_original_workday DATE,
-    emp_ID INT, 
-    replacement_emp INT, 
-    CONSTRAINT PK_Compensation_Leave PRIMARY KEY (request_ID),
-    CONSTRAINT FK_Compensation_Leave FOREIGN KEY (request_ID) REFERENCES Leave(request_ID),
-    CONSTRAINT FK_Compensation_Employee FOREIGN KEY (replacement_emp) REFERENCES Employee(employee_ID),
-    CONSTRAINT FK_Compensation_Replacement FOREIGN KEY(emp_ID) REFERENCES Employee(employee_ID)
+    emp_ID INT FOREIGN KEY  REFERENCES Employee(employee_ID), 
+    replacement_emp INT FOREIGN KEY REFERENCES Employee(employee_ID), 
+    PRIMARY KEY (request_ID)
 );
 
 CREATE TABLE Document(
-document_ID INT,
+document_ID INT PRIMARY KEY,
 type VARCHAR(50),
 description VARCHAR(50),
 file_name VARCHAR(50),
 creation_date DATE,
 expiry_date DATE,
 status VARCHAR(50),
-emp_ID INT,
-medical_ID INT,
-unpaid_ID INT,
-CHECK (status IN ('valid','expired')),
-CONSTRAINT PK_Document PRIMARY KEY (document_ID),
-CONSTRAINT FK_Document_Employee FOREIGN KEY (emp_ID) REFERENCES Employee(employee_ID),
-CONSTRAINT FK_Doc_Medical FOREIGN KEY(medical_ID) REFERENCES Medical_Leave(request_ID),
-CONSTRAINT FK_Doc_Unpaid FOREIGN KEY(unpaid_ID) REFERENCES Unpaid_Leave(request_ID)
+emp_ID INT FOREIGN KEY REFERENCES Employee(employee_ID),
+medical_ID INT FOREIGN KEY REFERENCES Medical_Leave(request_ID),
+unpaid_ID INT  FOREIGN KEY REFERENCES Unpaid_Leave(request_ID),
+CHECK (status IN ('valid','expired'))
 );
 
 CREATE TABLE Payroll (
-ID INT,
+ID INT PRIMARY KEY,
 payment_date DATE,
 final_salary_amount DECIMAL(10,1),
 from_date DATE,
 to_date DATE,
-comments VARCHAR(50),
+comments VARCHAR(150),
 bonus_amount DECIMAL(10,2),
 deductions_amount DECIMAL(10,2),
-emp_ID INT,
-CONSTRAINT PK_Payroll PRIMARY KEY(ID),
-CONSTRAINT FK_PAYROLL_EMPLOYEE FOREIGN KEY (emp_ID) REFERENCES Employee(employee_ID)
+emp_ID INT FOREIGN KEY  REFERENCES Employee(employee_ID)
 );
 
 CREATE TABLE Attendance(
-attendance_ID INT,
+attendance_ID INT PRIMARY KEY,
 date DATE,
 check_in_time TIME,
 check_out_time TIME,
 total_duration AS  DATEDIFF(MINUTE, check_in_time, check_out_time),
 status VARCHAR(50) DEFAULT 'Absent',
-emp_ID INT,
-CHECK(status IN ('Absent','Attended')),
-CONSTRAINT PK_Attendance PRIMARY KEY(attendance_ID),
-CONSTRAINT FK_Attendance_Emp FOREIGN KEY(emp_ID) REFERENCES Employee(employee_ID)
+emp_ID INT FOREIGN KEY REFERENCES Employee(employee_ID),
+CHECK(status IN ('Absent','Attended'))
 );
 
 CREATE TABLE deduction(
 deduction_ID INT,
-emp_ID INT,
+emp_ID INT FOREIGN KEY REFERENCES Employee(employee_ID),
 date DATE, 
 amount DECIMAL(10,2),
 type VARCHAR(50),
 status VARCHAR(50) DEFAULT 'pending',
-unpaid_ID INT,
-attendance_ID INT,
+unpaid_ID INT FOREIGN KEY REFERENCES Unpaid_Leave(request_ID),
+attendance_ID INT FOREIGN KEY REFERENCES Attendance(attendance_ID),
 CHECK(type IN ('unpaid','missing_hours','missing_days')),
 CHECK(status IN ('pending','finalized')),
-CONSTRAINT PK_deduction PRIMARY KEY (deduction_ID,emp_ID),
-CONSTRAINT FK_deduction_Emp FOREIGN KEY(emp_ID) REFERENCES Employee(employee_ID),
-CONSTRAINT FK_deduction_Unpaid FOREIGN KEY(unpaid_ID) REFERENCES Unpaid_Leave(request_ID),
-CONSTRAINT FK_deduction_Attendance FOREIGN KEY(attendance_ID) REFERENCES Attendance(attendance_ID)
+PRIMARY KEY (deduction_ID,emp_ID)
 );
 
 CREATE TABLE Performance (
-performance_ID int,
+performance_ID int PRIMARY KEY,
 rating INT,
 comments VARCHAR(50),
 semester CHAR(3),
-emp_ID int,
-CHECK(rating IN(1,2,3,4,5)),
-CONSTRAINT PK_Performance_P_ID PRIMARY KEY (performance_ID),
-CONSTRAINT FK_Performance_E_ID FOREIGN KEY (emp_ID) REFERENCES Employee(employee_ID)
+emp_ID int FOREIGN KEY REFERENCES Employee(employee_ID),
+CHECK(rating IN(1,2,3,4,5))
 );
 
 CREATE TABLE Employee_Replace_Employee (
-Emp1_ID int,
-Emp2_ID int,
+Emp1_ID int FOREIGN KEY  REFERENCES Employee(employee_ID),
+Emp2_ID int FOREIGN KEY  REFERENCES Employee(employee_ID),
 from_date DATE,
 to_date DATE,
-CONSTRAINT PK_Employee_Replace_Employe_PK PRIMARY KEY (Emp1_ID,Emp2_ID),
-CONSTRAINT FK_Employee_Replace_Employe_FK1 FOREIGN KEY ( Emp1_ID ) REFERENCES Employee(employee_ID),
-CONSTRAINT FK_Employee_Replace_Employe_FK2 FOREIGN KEY ( Emp2_ID ) REFERENCES Employee(employee_ID)
+PRIMARY KEY (Emp1_ID,Emp2_ID)
 );
 
 CREATE TABLE Employee_Approve_Leave (
-Emp1_ID int,
-Leave_ID int,
+Emp1_ID int FOREIGN KEY  REFERENCES Employee(employee_ID),
+Leave_ID int FOREIGN KEY  REFERENCES Leave(request_ID),
 status VARCHAR(50),
-CONSTRAINT PK_Employee_Approve_Leave_PK PRIMARY KEY ( Emp1_ID, Leave_ID ),
-CONSTRAINT FK_Employee_Approve_Leave_Emp1_ID FOREIGN KEY ( Emp1_ID ) REFERENCES Employee(employee_ID),
-CONSTRAINT FK_Employee_Approve_Leave_Leave_ID FOREIGN KEY ( Leave_ID ) REFERENCES Leave(request_ID)
+PRIMARY KEY ( Emp1_ID, Leave_ID )
 );
 END
 GO
@@ -300,6 +264,9 @@ DROP PROC clearAllTablesl
 END
 GO
 
+
+/*2.2 Samy starts here*/
+
 CREATE VIEW  allEmployeeProfiles 
 AS
 SELECT employee_ID, first_name,last_name,gender,email,address,years_of_experience,
@@ -331,9 +298,7 @@ CREATE VIEW allRejectedMedicals
 AS
 SELECT  m.request_ID,m.insurance_status, m.disability_details, m.type, m.Emp_ID,
 l.date_of_request,l.start_date,l.end_date,l.num_days, l.final_approval_status
-FROM Medical_Leave m
-INNER JOIN Leave l
-ON m.request_ID = l.request_ID
+FROM Medical_Leave m INNER JOIN Leave l ON m.request_ID = l.request_ID
 WHERE l.final_approval_status = 'rejected';
 GO
 SELECT * FROM  allPerformance
